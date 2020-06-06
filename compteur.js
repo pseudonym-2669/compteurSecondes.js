77f9ad1a9fedc54b2ea5932c70b5a7d7ef8e3d23
pseudonym-2669 <rob1.tisserand@gmail.com>
2020-06-06 21:57:41 +0200

    add minute and hours

--- compteur.js
+++ compteur.js
@@ -1,6 +1,24 @@
-var compteur = 1;
+var seconde = 1;
 var messageAffiche = function () {
-  console.log(compteur);
-  compteur++;
+  console.log(seconde);
+  seconde++;
+};
+var idIntervalle = setInterval(messageAffiche, 1000)
+
+
+var minute = 60;
+var messageAffiche = function () {
+  console.log(minute);
+  minute++;
+};
+var idIntervalle = setInterval(messageAffiche, 1000)
+
+
+
+
+var heure = 3600;
+var messageAffiche = function () {
+  console.log(heure);
+  heure++;
 };
 var idIntervalle = setInterval(messageAffiche, 1000)
