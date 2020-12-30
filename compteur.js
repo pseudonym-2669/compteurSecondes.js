d35916f88ffe43c1719506d392c09d5766387a48
pseudonym-2669 <55923733+pseudonym-2669@users.noreply.github.com>
2020-12-30 18:46:49 +0100

    Update compteur.js

--- compteur.js
+++ compteur.js
@@ -1,44 +1,10 @@
-var seconde = 1;
+ask = prompt("combien de secondes à compter ?")
+
+
+var seconde = ask;
 var messageAffiche = function () {
   console.log(seconde);
   seconde++;
-};
-var idIntervalle = setInterval(messageAffiche, 1000)
-
-
-var secondes = 2;
-var messageAffiche = function () {
-  console.log(secondes);
-  secondes++;
-};
-var idIntervalle = setInterval(messageAffiche, 1000)
-
-var minute = 60;
-var messageAffiche = function () {
-  console.log(minute);
-  minute++;
-};
-var idIntervalle = setInterval(messageAffiche, 1000)
-
-var TwoMinute = minute*2;
-var messageAffiche = function () {
-  console.log(TwoMinute);
-  TwoMinute++;
-};
-var idIntervalle = setInterval(messageAffiche, 1000)
-
-
-var heure = 3600;
-var messageAffiche = function () {
-  console.log(heure);
-  heure++;
-};
-var idIntervalle = setInterval(messageAffiche, 1000)
-
-var TwoHeure = heure*2;
-var messageAffiche = function () {
-  console.log(TwoHeure);
-  TwoHeure++;
 };
 var idIntervalle = setInterval(messageAffiche, 1000)
 
